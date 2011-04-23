bfe31d9f059320f77576c0c92bf3ab30e10cc3c3
Gareth Powell <gmmapowell@yahoo.com>
2011-04-23 08:28:43 -0400

    Added git comparison

--- Utils/src/main/java/com/gmmapowell/git/GitHelper.java
+++ Utils/src/main/java/com/gmmapowell/git/GitHelper.java
@@ -2,6 +2,7 @@ package com.gmmapowell.git;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 import java.io.PrintWriter;
@@ -13,7 +14,7 @@ import com.gmmapowell.utils.OrderedFileList;
 
 public class GitHelper {
 
-	public static boolean checkFiles(boolean b, OrderedFileList files, File file) {
+	public static boolean checkFiles(boolean doComparison, OrderedFileList files, File file) {
 		RunProcess proc = new RunProcess("git");
 		proc.executeInDir(FileUtils.getCurrentDir());
 		proc.captureStdout();
@@ -24,11 +25,22 @@ public class GitHelper {
 		}
 		proc.execute();
 		boolean dirty = false;
+		File newFile = null;
 		try
 		{
-			String out = proc.getStdout();
-			LineNumberReader r = new LineNumberReader(new StringReader(out));
-			FileOutputStream fos = new FileOutputStream(file);
+			doComparison &= file.exists();
+			LineNumberReader r = new LineNumberReader(new StringReader(proc.getStdout()));
+			LineNumberReader old = null;
+			FileOutputStream fos;
+			if (doComparison)
+			{
+				newFile = new File(file.getParentFile(), file.getName() + ".new");
+				fos = new FileOutputStream(newFile);
+				old = new LineNumberReader(new FileReader(file));
+			}
+			else
+				fos = new FileOutputStream(file);
+			
 			PrintWriter pw = new PrintWriter(fos);
 			for (File f : files)
 			{
@@ -36,15 +48,42 @@ public class GitHelper {
 				if (s == null)
 				{
 					dirty = true;
+					break;
 				}
-				pw.println(s + " " + FileUtils.makeRelative(f).getPath());
+				String nextLine = s + " " + FileUtils.makeRelative(f).getPath();
+				pw.println(nextLine);
+				if (old != null)
+				{
+					String o = old.readLine();
+					if (!o.equals(nextLine))
+					{
+						System.out.println("Files differ at file " + old.getLineNumber() +":");
+						System.out.println("  " + o);
+						System.out.println("  " + nextLine);
+						old.close();
+						old = null;
+						dirty = true;
+					}
+				}
 			}
 			pw.close();
 			fos.close();
+			if (doComparison)
+			{
+				if (dirty)
+				{
+					file.delete();
+					newFile.renameTo(file);
+				}
+				else
+					newFile.delete(); // by defn, they're the same, so remove the new one
+			}
 		}
 		catch (IOException ex)
 		{
 			dirty = true; // just call it dirty
+			file.delete();
+			newFile.delete();
 		}
 		return dirty;
 	}
