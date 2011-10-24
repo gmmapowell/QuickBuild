926c6f61804fda84e39171fdc25be7bbf7880505
Gareth Powell <gmmapowell@yahoo.com>
2011-10-24 05:42:04 -0400

    Local changes.

--- scripts/quickbuild.cmd
+++ scripts/quickbuild.cmd
@@ -1,2 +1,2 @@
-PATH=%PATH%;C:\cygwin\bin;C:\Program Files\Java\jdk1.6.0_25\bin
-bash /cygdrive/d/Personal/Tools/scripts/quickbuild.sh --cache=D:/Personal/%1-cache "%2"
+PATH=C:\Program Files\Java\jdk1.6.0_27\bin;C:\cygwin\bin;%PATH%
+bash /cygdrive/d/Personal/Tools/scripts/quickbuild.sh --cache=D:/Personal/%1-cache --build-all "%2"
