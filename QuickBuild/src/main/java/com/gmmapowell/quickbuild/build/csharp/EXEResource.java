48708297b4aafaf18688d97c27743b26644b9426
Gareth Powell <gmmapowell@yahoo.com>
2011-11-11 09:13:23 -0500

    Cleaning up warnings.

--- QuickBuild/src/main/java/com/gmmapowell/quickbuild/build/csharp/EXEResource.java
+++ QuickBuild/src/main/java/com/gmmapowell/quickbuild/build/csharp/EXEResource.java
@@ -2,10 +2,7 @@ package com.gmmapowell.quickbuild.build.csharp;
 
 import java.io.File;
 
-import com.gmmapowell.quickbuild.core.BuildResource;
-import com.gmmapowell.quickbuild.core.CloningResource;
 import com.gmmapowell.quickbuild.core.SolidResource;
-import com.gmmapowell.quickbuild.core.Strategem;
 
 public class EXEResource extends SolidResource {
 	public EXEResource(DevenvCommand devenvCommand, File file) {
