567600590c89a9260b4a6fc71edd8c9f25c38242
Gareth Powell <gmmapowell@yahoo.com>
2013-03-11 12:27:23 -0400

    Added --teamcity flag

--- scripts/quickbuild.cmd
+++ scripts/quickbuild.cmd
@@ -1,2 +1,2 @@
 PATH=C:\Program Files\Java\jdk1.7.0_11\bin;C:\cygwin\bin;%PATH%
-bash /cygdrive/d/Personal/Tools/scripts/quickbuild.sh --cache=D:/Personal/%1-cache --build-all "%2"
+bash /cygdrive/d/Personal/Tools/scripts/quickbuild.sh --cache=D:/Personal/%1-cache --build-all --teamcity "%2"
