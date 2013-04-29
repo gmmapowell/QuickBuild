7c3fa3431e21cce8f35d2b3c9f0c3c91dce2430d
Gareth Powell <gmmapowell@yahoo.com>
2013-04-29 11:20:57 -0400

    A "not implemented" exception

---
+++ Utils/src/main/java/com/gmmapowell/exceptions/NotImplementedException.java
@@ -0,0 +1,8 @@
+package com.gmmapowell.exceptions;
+
+@SuppressWarnings("serial")
+public class NotImplementedException extends UtilException {
+	public NotImplementedException() {
+		super("Not Implemented");
+	}
+}
