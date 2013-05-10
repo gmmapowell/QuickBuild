c1c3372294bd8823d04761204b4c320b80977984
Gareth Powell <gmmapowell@yahoo.com>
2013-05-10 10:53:47 -0400

    Improved JSON parser.

--- Utils/src/main/java/com/gmmapowell/parser/SaneJSONParser.java
+++ Utils/src/main/java/com/gmmapowell/parser/SaneJSONParser.java
@@ -1,6 +1,7 @@
 package com.gmmapowell.parser;
 
 import java.io.Reader;
+import java.util.List;
 
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonParser;
@@ -58,10 +59,18 @@ public class SaneJSONParser {
 		accept();
 	}
 	
+	/** Check if the current token matches the possible token, but do NOT accept it
+	 * @param possible the token to compare to
+	 * @return true if the current token matches the sample; false otherwise
+	 */
 	public boolean is(JsonToken possible) {
 		return possible == get();
 	}
 
+	/** Check if the current token matches the possible token, and accept it if true
+	 * @param possible the token to compare to
+	 * @return true if the current token matches the sample; false otherwise
+	 */
 	public boolean match(JsonToken possible) {
 		boolean ret = possible == get();
 		if (ret)
@@ -69,6 +78,12 @@ public class SaneJSONParser {
 		return ret;
 	}
 
+	/** Check if the current token matches the possible token, and accept it if true.
+	 * Only consider it a match if the current text node also matches the specified text.
+	 * 
+	 * @param possible the token to compare to
+	 * @return true if the current token matches the sample; false otherwise
+	 */
 	public boolean match(JsonToken possible, String withText) {
 		boolean ret = possible == get();
 		if (!ret)
@@ -79,6 +94,10 @@ public class SaneJSONParser {
 		return true;
 	}
 
+	/** Check that the current token is a field name and return the name of the field
+	 * 
+	 * @return the name of the next field
+	 */
 	public String getFieldName() {
 		JsonToken tok = get();
 		if (tok != JsonToken.FIELD_NAME)
@@ -89,6 +108,10 @@ public class SaneJSONParser {
 	}
 
 	@SuppressWarnings("unchecked")
+	/** Extract the value of the current token/field
+	 * 
+	 * @return the converted value of the current token's text.
+	 */
 	public <T> T extractField() {
 		JsonToken tok;
 		tok = get();
@@ -103,18 +126,37 @@ public class SaneJSONParser {
 			ret = (T)(Boolean)true;
 		else if (tok == JsonToken.VALUE_FALSE)
 			ret = (T)(Boolean)false;
+		else if (tok == JsonToken.VALUE_NULL)
+			ret = null;
 		else
 			throw new UtilException("That is not handled: " + tok);
 		accept();
 		return ret;
 	}
 
+	/** Extract the value of the next field
+	 * 
+	 * @param field the name of the current field, which must match
+	 * @return the value of the named field
+	 */
 	public <T> T extractField(String field) {
 		JsonToken tok = get();
 		if (tok != JsonToken.FIELD_NAME || !text.equals(field))
 			throw new UtilException("Expected FIELD_NAME (" + field + ") but was '" + tok + "' (" + text + ")");
 		accept();
 		return extractField();
+	}
+
+	/** Read a list of strings from the input (as an array) and store in the provided list
+	 * 
+	 * @param into a list of strings to append to
+	 */
+	public void readStringList(List<String> into) {
+		match(JsonToken.START_ARRAY);
+		while (!is(JsonToken.END_ARRAY)) {
+			into.add((String)extractField());
+		}
+		match(JsonToken.END_ARRAY);
 	}
 
 	private JsonToken get() {
@@ -137,5 +179,9 @@ public class SaneJSONParser {
 
 	public void close() {
 		// Close any open readers, etc ...
+	}
+
+	public String currentTok() {
+		return ctok.toString() + "[" + text + "]";
 	}
 }
