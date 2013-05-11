c0ad3764056df3fe15057e42be11c1302758311f
Gareth Powell <gmmapowell@yahoo.com>
2013-05-11 10:05:28 -0400

    Added forcing moves to make it complete for most cases.

--- Utils/src/main/java/com/gmmapowell/apps/SchedulePlanner2.java
+++ Utils/src/main/java/com/gmmapowell/apps/SchedulePlanner2.java
@@ -7,7 +7,6 @@ import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
-import com.gmmapowell.apps.SchedulePlanner2.Round;
 import com.gmmapowell.collections.CollectionUtils;
 import com.gmmapowell.exceptions.UtilException;
 import com.gmmapowell.utils.Justification;
@@ -29,8 +28,8 @@ public class SchedulePlanner2 {
 		}
 		boolean bothWays = Boolean.parseBoolean(args[1]);
 		
-		int k = n;
-//		for (int k = 1;k<30; k+=2)
+//		int k = n;
+		for (int k = 1;k<26; k++)
 		{
 			SchedulePlanner2 planner = new SchedulePlanner2(k, bothWays);
 			Schedule s = planner.findFixtures();
@@ -63,18 +62,178 @@ public class SchedulePlanner2 {
 	private Schedule findFixtures() {
 		Schedule ret = new Schedule(nteams, bothWays);
 		FixtureList fixtures = new FixtureList(teams, bothWays);
-		List<Fixture> broken = new ArrayList<Fixture>();
-		Fixture next;
-		while ((next = fixtures.choose()) != null) {
-			if (!place(ret, next))
-				broken.add(next);
-		}
-		if (broken.isEmpty())
-			return ret;
+		List<Fixture> broken;
+		while (true) {
+			while (true) {
+				// The simplest thing is just trying to slot fixtures in where we can
+				broken = new ArrayList<Fixture>();
+				Fixture next;
+				while ((next = fixtures.choose()) != null) {
+					if (!place(ret, next))
+						broken.add(next);
+				}
+				if (broken.isEmpty())
+					return ret;
+				
+//				System.out.println(ret);
+//				System.out.println(broken);
 		
-		System.out.println(ret);
-		System.out.println(broken);
-		throw new UtilException("There were " + broken.size() + " fixtures that could not be placed");
+				// When that fails, try to move the fixtures around and see what happens ...
+				// This should have the effect of making the "left hand side" more complete
+				boolean didSomething = tryShufflingIncompletes(ret, broken);
+				
+				if (broken.isEmpty())
+					return ret;
+				
+//				System.out.println(ret);
+//				System.out.println(broken);
+				fixtures = new FixtureList(broken);
+				
+				if (!didSomething)
+					break;
+			}
+			
+			// now be more assertive
+			// find an item from the broken list which can forcibly displace something somewhere so that
+			// it can take its place.
+			// The something must have one but not both of the same teams
+			// The somewhere must be an incomplete row
+			
+			if (!tryForcibleMoves(ret, broken))
+				break;
+		}
+		// Finally, just try random persuasion
+		// TODO: I think a breadth-first search for possible chains of rearrangement would have to be more efficient
+		int cnt = 10000000;
+		loop:
+		for (int i=0;i<cnt;i++) {
+			if (broken.isEmpty()) {
+				System.out.println("Finished after " + i + " iterations");
+				return ret;
+			}
+			Fixture f = broken.remove(0);
+			if (place(ret, f)) {
+//				System.out.println("Placed " + f);
+				continue;
+			}
+			// Look for volunteers
+			for (Round r : ret.rounds) {
+				Fixture h = r.fixtureFor(f.home);
+				Fixture a = r.fixtureFor(f.away);
+				if (h != null && a == null && place(ret, h)) {
+					r.removeFixture(h);
+					r.add(f);
+					continue loop;
+				} else if (h == null && a != null && place(ret, a)) {
+					r.removeFixture(a);
+					r.add(f);
+					continue loop;
+				}
+			}
+			{
+				// OK, hack it
+				Round r = ret.rounds.get(random.nextInt(ret.rounds.size()));
+				Fixture h = r.fixtureFor(f.home);
+				Fixture a = r.fixtureFor(f.away);
+				if (h != null) {
+					r.removeFixture(h);
+					broken.add(h);
+				}
+				if (a != null && a != h) {
+					r.removeFixture(a);
+					broken.add(a);
+				}
+				r.add(f);
+			}
+//			System.out.println(ret);
+//			System.out.println(broken);
+		}
+		throw new UtilException("There were " + broken.size() + " fixtures that could not be placed after " + cnt + " iterations");
+	}
+
+	private boolean tryShufflingIncompletes(Schedule ret, List<Fixture> broken) {
+		boolean didSomething = false;
+		
+		List<Round> incomplete = figureIncompletes(ret);
+		
+		// For each round, try and find its missing fixture somewhere else
+		for (Round r : incomplete) {
+			List<String> toPlace = r.teamsMissing();
+//			System.out.println(r.name + toPlace);
+			for (Round s : incomplete) {
+				if (r == s)
+					continue;
+				List<String> sMissing = s.teamsMissing();
+				// Don't steal from someone in better shape than us
+				if (sMissing.size() < toPlace.size())
+					continue;
+				for (String[] p : allPairs(toPlace)) {
+					Fixture f;
+					if ((f = s.hasFixture(p[0], p[1])) != null) {
+						s.removeFixture(f);
+						r.add(f);
+						toPlace.remove(p[0]);
+						toPlace.remove(p[1]);
+						didSomething = true;
+					}
+				}
+			}
+		}
+		
+		return didSomething;
+	}
+
+	private boolean tryForcibleMoves(Schedule ret, List<Fixture> broken) {
+		List<Round> incomplete = figureIncompletes(ret);
+
+		// The moment we do anything, return true
+		for (Fixture f : broken) {
+			for (Round r : incomplete) {
+				List<String> missing = r.teamsMissing();
+				Fixture g = null;
+				if (missing.contains(f.home) && !missing.contains(f.away))
+					g = r.fixtureFor(f.home);
+				else if (missing.contains(f.away) && !missing.contains(f.home))
+					g = r.fixtureFor(f.away);
+				if (g == null)
+					continue;
+				// OK, it has one but not the other ... would fixture g equally happy elsewhere?
+				for (Round s : incomplete) {
+					if (s == r)
+						continue;
+					if (!s.hasTeam(g.home) && !s.hasTeam(g.away)) {
+						s.add(g);
+						r.removeFixture(g);
+						r.add(f);
+						return true;
+					}
+				}
+			}
+		}
+		
+		// If we get here, we didn't manage to find anything to do
+		return false;
+	}
+
+	private List<Round> figureIncompletes(Schedule ret) {
+		// Find all the incomplete rounds
+		List<Round> incomplete = new ArrayList<Round>();
+		for (Round r : ret.rounds) {
+			List<String> toPlace = r.teamsMissing();
+			if (toPlace.isEmpty())
+				continue;
+			else
+				incomplete.add(r);
+		}
+		return incomplete;
+	}
+	
+	private List<String[]> allPairs(List<String> toPlace) {
+		List<String[]> ret = new ArrayList<String[]>();
+		for (int i=0;i<toPlace.size();i++)
+			for (int j=i+1;j<toPlace.size();j++)
+				ret.add(new String[] { toPlace.get(i), toPlace.get(j) });
+		return ret;
 	}
 
 	private boolean place(Schedule ret, Fixture next) {
@@ -107,7 +266,7 @@ public class SchedulePlanner2 {
 				
 			rounds = new ArrayList<Round>(nrounds);
 			for (int i=0;i<nrounds;i++)
-				rounds.add(new Round());
+				rounds.add(new Round(i+1));
 		}
 
 		public Set<Round> availableFor(String t) {
@@ -120,7 +279,7 @@ public class SchedulePlanner2 {
 
 		private void dump(PrettyPrinter pp) {
 			dumpTitles(pp);
-			for (int i=0;i<nteams;i++) {
+			for (int i=0;i<(nteams+1)/2;i++) {
 				for (Round r : rounds)
 					r.dump(pp, i);
 				pp.requireNewline();
@@ -128,9 +287,8 @@ public class SchedulePlanner2 {
 		}
 
 		private void dumpTitles(PrettyPrinter pp) {
-			int w = 1;
-			for (@SuppressWarnings("unused") Round r : rounds)
-				pp.append(Justification.LEFT.format("Wk"+(w++), 5));
+			for (Round r : rounds)
+				pp.append(Justification.LEFT.format(r.name, 5));
 			pp.requireNewline();
 		}
 
@@ -158,6 +316,10 @@ public class SchedulePlanner2 {
 					for (int j=i+1;j<teams.size();j++)
 						fixtures.add(new Fixture(teams.get(i), teams.get(j)));
 			}
+		}
+
+		public FixtureList(List<Fixture> broken) {
+			this.fixtures = broken;
 		}
 
 		public Fixture choose() {
@@ -201,16 +363,51 @@ public class SchedulePlanner2 {
 
 	public class Round {
 		private Set<Fixture> fixtures = new TreeSet<Fixture>();
+		private final String name;
 		
+		public Round(int w) {
+			name = "Wk" + w;
+		}
+
 		public void add(Fixture next) {
 			fixtures.add(next);
 		}
 		
+		public void removeFixture(Fixture f) {
+			if (!fixtures.remove(f))
+				throw new UtilException(this + " did not have " + f);
+		}
+		
+		public List<String> teamsMissing() {
+			List<String> ret = new ArrayList<String>(teams);
+			for (Fixture f : fixtures) {
+				ret.remove(f.home);
+				ret.remove(f.away);
+			}
+			return ret;
+		}
+
 		public boolean hasTeam(String team) {
 			for (Fixture f : fixtures)
 				if (f.home.equals(team) || f.away.equals(team))
 					return true;
 			return false;
+		}
+
+		public Fixture hasFixture(String t1, String t2) {
+			for (Fixture f : fixtures)
+				if (f.home.equals(t1) && f.away.equals(t2))
+					return f;
+				else if (f.home.equals(t2) && f.away.equals(t1))
+					return f;
+			return null;
+		}
+
+		public Fixture fixtureFor(String t) {
+			for (Fixture f : fixtures)
+				if (f.home.equals(t) || f.away.equals(t))
+					return f;
+			return null;
 		}
 
 		public void dump(PrettyPrinter pp, int i) {
@@ -222,5 +419,9 @@ public class SchedulePlanner2 {
 			pp.append(Justification.LEFT.format(show, 5));
 		}
 
+		@Override
+		public String toString() {
+			return name;
+		}
 	}
 }
