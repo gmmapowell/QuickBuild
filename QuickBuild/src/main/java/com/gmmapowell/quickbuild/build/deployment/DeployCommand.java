d60c0231637f11f61cb396f19d2f629631462fff
Gareth Powell <gareth@ziniki.org>
2017-02-10 05:39:39 -0500

    Improved deploy command

--- QuickBuild/src/main/java/com/gmmapowell/quickbuild/build/deployment/DeployCommand.java
+++ QuickBuild/src/main/java/com/gmmapowell/quickbuild/build/deployment/DeployCommand.java
@@ -8,40 +8,38 @@ import java.util.Map;
 
 import org.zinutils.exceptions.UtilException;
 import org.zinutils.parser.TokenizedLine;
-import com.gmmapowell.quickbuild.build.BuildContext;
-import com.gmmapowell.quickbuild.build.BuildStatus;
-import com.gmmapowell.quickbuild.config.Config;
-import com.gmmapowell.quickbuild.config.ConfigApplyCommand;
-import com.gmmapowell.quickbuild.core.AbstractStrategemTactic;
-import com.gmmapowell.quickbuild.core.BuildResource;
-import com.gmmapowell.quickbuild.core.PendingResource;
-import com.gmmapowell.quickbuild.core.ResourcePacket;
-import com.gmmapowell.quickbuild.core.Strategem;
 import org.zinutils.utils.ArgumentDefinition;
 import org.zinutils.utils.Cardinality;
 import org.zinutils.utils.FileUtils;
 import org.zinutils.utils.OrderedFileList;
 
-public class DeployCommand extends AbstractStrategemTactic {
+import com.gmmapowell.quickbuild.build.BuildContext;
+import com.gmmapowell.quickbuild.build.BuildStatus;
+import com.gmmapowell.quickbuild.config.AbstractBuildCommand;
+import com.gmmapowell.quickbuild.config.Config;
+import com.gmmapowell.quickbuild.config.ConfigApplyCommand;
+import com.gmmapowell.quickbuild.core.BuildResource;
+import com.gmmapowell.quickbuild.core.PendingResource;
+import com.gmmapowell.quickbuild.core.ResourcePacket;
+import com.gmmapowell.quickbuild.core.Strategem;
+
+public class DeployCommand extends AbstractBuildCommand {
+	private boolean unnest = false;
 	private String target;
-	private List<String> artifacts = new ArrayList<String>();
+	private final List<String> artifacts = new ArrayList<String>();
 	private ResourcePacket<PendingResource> needs = new ResourcePacket<PendingResource>();
 	private ResourcePacket<BuildResource> builds = new ResourcePacket<BuildResource>();
 	private Map<String, PendingResource> pendings = new HashMap<String, PendingResource>();
 	private Map<String, BuildResource> buildings = new HashMap<String, BuildResource>();
-	private final List<ConfigApplyCommand> options = new ArrayList<ConfigApplyCommand>();
+	private DeployedObject firstBR;
 
+	@SuppressWarnings("unchecked")
 	public DeployCommand(TokenizedLine toks) {
-		super(toks, 
+		toks.process(this, 
+				new ArgumentDefinition("--unnest", Cardinality.OPTION, "unnest", "do not copy directory tree"),
 				new ArgumentDefinition("*", Cardinality.REQUIRED, "target", "deploy target"),
 				new ArgumentDefinition("*", Cardinality.ONE_OR_MORE, "artifacts", "artifacts")
 		);
-	}
-
-
-	@Override
-	public void addChild(ConfigApplyCommand obj) {
-		options.add(obj);
 	}
 
 	@Override
@@ -52,20 +50,30 @@ public class DeployCommand extends AbstractStrategemTactic {
 			pendings.put(i, pr);
 			needs.add(pr);
 			
-			DeployedObject dobj = new DeployedObject(this, FileUtils.relativePath(new File(config.getPath(target), i)));
+			String to = i;
+			if (unnest)
+				to = new File(to).getName();
+			DeployedObject dobj = new DeployedObject(this, FileUtils.relativePath(new File(config.getPath(target), to)));
+			if (firstBR == null)
+				firstBR = dobj;
 			buildings.put(i, dobj);
 			builds.add(dobj);
 		}
 		
-		for (ConfigApplyCommand cmd : options)
-		{
-			if (cmd instanceof AfterCommand)
-				needs.add(((AfterCommand)cmd).getAfter());
-			else
-				throw new UtilException("Unrecognized Command: " + cmd);
-		}
+		super.handleOptions(config);
 
 		return this;
+	}
+	
+	@Override
+	public boolean handleOption(Config config, ConfigApplyCommand cmd) {
+		if (super.handleOption(config, cmd))
+			return true;
+		else if (cmd instanceof AfterCommand) {
+			needs.add(((AfterCommand)cmd).getAfter());
+			return true;
+		} else
+			throw new UtilException("Unrecognized Command: " + cmd);
 	}
 
 	@Override
@@ -84,15 +92,16 @@ public class DeployCommand extends AbstractStrategemTactic {
 		for (String i : artifacts)
 		{
 			File from = pendings.get(i).getPath();
-			FileUtils.copyAssertingDirs(from, new File(deployTo, i));
-			cxt.builtResource(buildings.get(i));
+			BuildResource br = buildings.get(i);
+			FileUtils.copyAssertingDirs(from, br.getPath());
+			cxt.builtResource(br);
 		}
 		return BuildStatus.SUCCESS;
 	}
 
 	@Override
 	public String identifier() {
-		return "Deploy["+target+"]";
+		return "Deploy["+firstBR.getPath()+"]";
 	}
 
 	@Override
